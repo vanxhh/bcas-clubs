8c50940518b388f4caa27ed0306568291d4334ba
vansshhhh <bhardwajvansh26@gmail.com>
2022-04-27 14:24:51 +0530

    sendfile_revert

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use('/api', router)
 	}));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	res.render(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.get('*', (req, res) => {
